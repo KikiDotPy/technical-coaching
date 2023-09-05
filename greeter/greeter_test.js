3e16f2c06ed4004b00716d1bc4e81dfa009dca9c
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-09-05 17:40:52 +0200

    Rony, Kiki | Step 2 - add a specific greeting depending on the time of day

--- greeter/greeter_test.js
+++ greeter/greeter_test.js
@@ -2,16 +2,24 @@
 
 const Greeter = require('./greeter.js')
 
-test('greeter can greet Chiara', () => {
+test('greeter can greet Chiara in the evening', () => {
 
     const greeting = new Greeter().greet('Chiara');
 
-    expect(greeting).toBe('Hello, Chiara');
+    expect(greeting).toBe('Good evening, Chiara');
 });
 
-test('greeter can greet Rony', () => {
+test('greeter can greet Rony in the evening', () => {
 
     const greeting = new Greeter().greet('Rony');
 
-    expect(greeting).toBe('Hello, Rony');
+    expect(greeting).toBe('Good evening, Rony');
+});
+
+test('greeter can greet Marco in the morning', () => {
+    const thisMorning =  new Date('2023-09-05T11:00:00');
+
+    const greeting = new Greeter(thisMorning).greet('Marco');
+
+    expect(greeting).toBe('Good morning, Marco');
 });
