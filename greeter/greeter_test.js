e7b669f91d882311eb238ee3a25d6b8419501db3
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-09-12 16:49:43 +0200

    Kiki, Rony | Clean up tests

--- greeter/greeter_test.js
+++ greeter/greeter_test.js
@@ -2,19 +2,6 @@
 
 const Greeter = require('./greeter.js')
 
-test('greeter can greet Chiara in the evening', () => {
-
-    const greeting = new Greeter().greet('Chiara');
-
-    expect(greeting).toBe('Good evening, Chiara');
-});
-
-test('greeter can greet Rony in the evening', () => {
-
-    const greeting = new Greeter().greet('Rony');
-
-    expect(greeting).toBe('Good evening, Rony');
-});
 
 test('greeter can greet Marco in the morning', () => {
     const thisMorning =  new Date('2023-09-05T11:00:00');
