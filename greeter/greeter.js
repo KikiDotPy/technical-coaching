92b355e35fb01625139b2502c2f7053c42e2942e
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-09-12 17:36:14 +0200

    Rony, Kiki | Add good night and good evening greeting

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -1,8 +1,20 @@
 'use strict';
 
+function isMorningHour(currentHours) {
+    return currentHours >= 6 && currentHours < 12;
+}
+
+function isAfternoonHour(currentHours) {
+    return currentHours >= 18 && currentHours < 22;
+}
+
+function isNightHour(currentHours) {
+    return currentHours >= 22 || currentHours < 6;
+}
+
 class Greeter {
     constructor(date = new Date()) {
-        this.date= date;
+        this.date = date;
     }
 
     greet(name) {
@@ -23,8 +35,14 @@ class Greeter {
     getSalutation() {
         const currentHours = this.date.getHours()
 
-        if (currentHours >= 6 && currentHours < 12){
+        if (isMorningHour(currentHours)) {
             return 'Good morning'
+        }
+        if (isAfternoonHour(currentHours)) {
+            return 'Good evening'
+        }
+        if (isNightHour(currentHours)) {
+            return 'Good night'
         }
 
         return 'Hello'
--- greeter/greeter_test.js
+++ greeter/greeter_test.js
@@ -42,3 +42,26 @@ test('greet uses "Hello" after lunch', () => {
     expect(greeting).toBe('Hello, Marisa');
 });
 
+test('greet uses "Good evening" when the time is 18:00-22:00', () => {
+    const eveningTime = new Date('2023-09-07T18:30:00')
+
+    const greeting = new Greeter(eveningTime).greet('Garrus');
+
+    expect(greeting).toBe('Good evening, Garrus');
+});
+
+test('greet uses "Good night" when the time is after 22', () => {
+    const bedTime = new Date('2023-09-07T23:30:00')
+
+    const greeting = new Greeter(bedTime).greet('Andariel');
+
+    expect(greeting).toBe('Good night, Andariel');
+});
+
+test('greet uses "Good night" when the time is before 6', () => {
+    const bedTime = new Date('2023-09-07T03:30:00')
+
+    const greeting = new Greeter(bedTime).greet('Andariel');
+
+    expect(greeting).toBe('Good night, Andariel');
+});
