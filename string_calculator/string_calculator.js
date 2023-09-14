fb07bdc63f3c38846f51751edb78a5c211eb9051
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-09-14 16:03:29 +0200

    Kiki, Rony | Basic version of add function

--- string_calculator/string_calculator.js
+++ string_calculator/string_calculator.js
@@ -1,6 +1,12 @@
 'use strict';
 
 class StringCalculator {
+    add(string) {
+        if (string === '') {
+            return 0;
+        }
+        return Number(string);
+    }
 }
 
 module.exports = StringCalculator
--- string_calculator/string_calculator_test.js
+++ string_calculator/string_calculator_test.js
@@ -2,6 +2,28 @@
 
 const StringCalculator = require('./string_calculator.js')
 
-test('test runner is set up correctly', () => {
-    expect(0).toBe(1);
+test('StringCalculator return 0 when a string is empty', () => {
+    const stringCalculator = new StringCalculator();
+
+    const addResult = stringCalculator.add('')
+
+    expect(addResult).toBe(0);
+});
+
+// A single number returns the value `'1' => 1` `'2' => 2`
+
+test('StringCalculator when given string "1", returns its value', () => {
+    const stringCalculator = new StringCalculator();
+
+    const addResult = stringCalculator.add('1')
+
+    expect(addResult).toBe(1);
+});
+
+test('StringCalculator when given string "2", returns its value', () => {
+    const stringCalculator = new StringCalculator();
+
+    const addResult = stringCalculator.add('2')
+
+    expect(addResult).toBe(2);
 });
